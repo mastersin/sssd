4826235aebbf52cc5b85fa657d2a95bb1e3ba360
Lukas Slebodnik <lslebodn@redhat.com>
2014-03-14 17:59:52 +0100

    UTIL: Ignore insufficient permission for some proc files
    proc filesystem can be mount with option hidepid=1.
    In this case, we do not have permisions for reading some files
    /proc/$(pid)/status; just ignore them.
    
    Signed-off-by: Alexey Shabalin <shaba@altlinux.ru>

--- src/util/find_uid.c
+++ src/util/find_uid.c
@@ -234,6 +234,9 @@ static errno_t get_active_uid_linux(hash_table_t *table, uid_t search_uid)
         }
 
         ret = get_uid_from_pid(pid, &uid);
+        if (ret == EPERM) {
+            continue;
+        }
         if (ret != EOK) {
             DEBUG(SSSDBG_CRIT_FAILURE, "get_uid_from_pid failed.\n");
             goto done;
