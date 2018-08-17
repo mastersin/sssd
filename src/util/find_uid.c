3dba390f989069a827d62b7b80f7495daf8aff29
Alexey Sheplyakov <asheplyakov@altlinux.org>
2018-08-17 16:46:13 +0400

    Revert "Fix "test-find-uid" test" due to security issues
    TL;DR: krb5 provider does need to find processes of users being
    authenticated, therefore it can't operate with a broken /proc (with
    unreadable entries).
    
    1. krb5_child uses `check_if_uid_is_active` to decide if the old credentials
       cache can be reused, the call stack looks like this:
    
       get_uid_from_pid
       get_active_uid_linux
       check_if_uid_is_active
       k5c_check_old_ccache
       k5c_ccache_setup
    
       With `fix "test-find-uid" test' `get_uid_from_pid` can return EOK
       without setting the `uid`. As a result `get_active_uid_linux` read
       uninitialized value of `uid` *and* insert that random value into
       `uid_table`. That's definitely a bad thing.
    
    2. krb5 provider uses `get_uid_table` in the delayed online authentication
       process:
    
       get_active_uid_linux
       get_uid_table
       authenticate_stored_users
       delayed_online_authentication_callback
    
       With `fix "test-find-uid" test' `get_uid_table` insert garbage for
       every unreadable proc entry, thus `authenticate_stored_users` won't
       even try to authenticate some (still logged in) users. That's just
       plain wrong.
    
    Thus commit `fix "test-find-uid" test' [1] is wrong and should be reverted.
    Ignoring errors just to get the test pass in a broken environment is
    a wrong approach.
    
    [1] original commit ID: 61522a6e4953dd08abfff0f7d8f5b3d630342cb8

--- src/util/find_uid.c
+++ src/util/find_uid.c
@@ -96,13 +96,6 @@ static errno_t get_uid_from_pid(const pid_t pid, uid_t *uid)
                       path);
             return EOK;
         }
-        // case of hidepid mount option for /proc
-        if (error == EPERM) {
-            DEBUG(SSSDBG_TRACE_LIBS,
-                  "Proc file [%s] is not permissible, continuing.\n",
-                      path);
-            return EOK;
-        }
         DEBUG(SSSDBG_CRIT_FAILURE,
               "open failed [%s][%d][%s].\n", path, error, strerror(error));
         return error;
